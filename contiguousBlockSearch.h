97884bb6cc0d3797a8686f4f726da91defa22466
Leonardo Borrero Luz <leonardo_borrero_luz@live.com>
2013-09-28 00:31:47 -0400

    can now detect strait row or column matches

--- contiguousBlockSearch.h
+++ contiguousBlockSearch.h
@@ -28,15 +28,15 @@ public:
 	
 	static void recursiveRight(vector<int>& idCollector, int arrs[], int counter, int width, int height){
 		int checkerInt = (counter+1)%(width+1);
-		cout << "recursiveRight: " << checkerInt << endl;
+		//cout << "recursiveRight: " << checkerInt << endl;
 		if (arrs[counter] == 0 || checkerInt == 0 || counter > (width*height)-1) {
 			return;
 		}else{
-			cout << &idCollector << " ";
+			//cout << &idCollector << " ";
 			if(!verifyDouble(idCollector, counter)){
 				idCollector.push_back(counter);
 			}
-			cout << counter << ": " << arrs[counter] << endl;
+			//cout << counter << ": " << arrs[counter] << endl;
 			recursiveRight(idCollector, arrs, ++counter, width, height);
 			return;
 		}
@@ -44,30 +44,30 @@ public:
 	
 	static void recursiveLeft(vector<int>& idCollector, int arrs[], int counter, int width, int height){
 		int checkerInt = (counter+1)%(width+1);
-		cout << "recursiveLeft: " << checkerInt << endl;
+		//cout << "recursiveLeft: " << checkerInt << endl;
 		if (arrs[counter] == 0 || checkerInt == 0) {
 			return;
 		}else{
-			cout << &idCollector << " ";
+			//cout << &idCollector << " ";
 			if(!verifyDouble(idCollector, counter)){
 				idCollector.push_back(counter);
 			}
-			cout << counter << ": " << arrs[counter] << endl;
+			//cout << counter << ": " << arrs[counter] << endl;
 			recursiveLeft(idCollector, arrs, --counter, width, height);
 			return;
 		}
 	}
 	
 	static void recursiveTop(vector<int>& idCollector, int arrs[], int counter, int width, int height){
-		cout << "recursiveTop: " << counter << endl;
+		//cout << "recursiveTop: " << counter << endl;
 		if (arrs[counter] == 0 || counter < 0) {
 			return;
 		}else{
-			cout << &idCollector << " ";
+			//cout << &idCollector << " ";
 			if(!verifyDouble(idCollector, counter)){
 				idCollector.push_back(counter);
 			}
-			cout << counter << ": " << arrs[counter] << endl;
+			//cout << counter << ": " << arrs[counter] << endl;
 			counter = counter - width;
 			recursiveTop(idCollector, arrs, counter, width, height);
 			return;
@@ -75,15 +75,15 @@ public:
 	}
 	
 	static void recursiveBottom(vector<int>& idCollector, int arrs[], int counter, int width, int height){
-		cout << "recursiveBottom: " << counter << endl;
+		//cout << "recursiveBottom: " << counter << endl;
 		if (arrs[counter] == 0 || counter > (width*height)-1) {
 			return;
 		}else{
-			cout << &idCollector << " ";
+			//cout << &idCollector << " ";
 			if(!verifyDouble(idCollector, counter)){
 				idCollector.push_back(counter);
 			}
-			cout << counter << ": " << arrs[counter] << endl;
+			//cout << counter << ": " << arrs[counter] << endl;
 			counter = counter + width;
 			recursiveBottom(idCollector, arrs, counter, width, height);
 			return;
@@ -107,7 +107,93 @@ public:
 	
 	static int indexToCoordY(int coordIndex, int width){
 		return floor(coordIndex/width);
-	};
+	}
+	
+	static int giveMeRowMatchesForCoord(vector<int> &ids, int xPos, int yPos, int width, int height){
+		vector<int> returningIds;
+		
+		int goodId = coordToIndex(xPos, yPos, width);
+		bool goodIdDetected = false;
+		bool breakEverything = false;
+		
+		int initialCountPoint = yPos*width;
+		int finalCountPoint = (yPos+1)*width - 1;
+		
+		//cout << initialCountPoint << " to " << finalCountPoint << endl;
+		
+		for (int i=initialCountPoint; i<finalCountPoint; i++) {
+			bool inIds = false;
+			for (int j=0; j<ids.size(); j++) {
+				if (i == ids.at(j)) {
+					inIds = true;
+					//cout << "inIds: " << inIds << endl;
+				}
+			}
+			if (inIds) {
+				if (goodId == i) {
+					goodIdDetected = true;
+					//cout << "goodIdDetected: " << i << endl;
+				}
+				if(!breakEverything){
+					returningIds.push_back(i);
+				}
+				//cout << "push_back: " << i << endl;
+			}else{
+				if (goodIdDetected) {
+					//cout << "goodIdDetected check" << endl;
+					breakEverything = true;
+				}else{
+					//cout << "clearing: " << returningIds.size() << endl;
+					returningIds.clear();
+				}
+			}
+		}
+		
+		return returningIds.size();
+	}
+	
+	static int giveMeColomnMatchesForCoord(vector<int> &ids, int xPos, int yPos, int width, int height){
+		vector<int> returningIds;
+		
+		int goodId = coordToIndex(xPos, yPos, width);
+		bool goodIdDetected = false;
+		bool breakEverything = false;
+		
+		int initialCountPoint = xPos;
+		int finalCountPoint = xPos+((height-1)*width);
+		
+		//cout << initialCountPoint << " to " << finalCountPoint << endl;
+		
+		for (int i=initialCountPoint; i<finalCountPoint; i+=width) {
+			bool inIds = false;
+			for (int j=0; j<ids.size(); j++) {
+				if (i == ids.at(j)) {
+					inIds = true;
+					//cout << "inIds: " << i << endl;
+				}
+			}
+			if (inIds) {
+				if (goodId == i) {
+					goodIdDetected = true;
+					//cout << "goodIdDetected: " << i << endl;
+				}
+				if(!breakEverything){
+					returningIds.push_back(i);
+				}
+				//cout << "push_back: " << i << endl;
+			}else{
+				if (goodIdDetected) {
+					//cout << "goodIdDetected check" << endl;
+					breakEverything = true;
+				}else{
+					//cout << "clearing: " << returningIds.size() << endl;
+					returningIds.clear();
+				}
+			}
+		}
+		
+		return returningIds.size();
+	}
 	
 	static vector<int> returnContiguousFromTile(int intArray2[], int width2, int height2, int initialCoord_x2, int initialCoord_y2){
 		int width = 10;
@@ -116,15 +202,15 @@ public:
 		int initialCoord_x = 4;
 		int initialCoord_y = 2;
 		
-		int intArray[]={1, 1, 1, 0, 0, 0, 1, 1, 1, 0,
-						0, 0, 1, 1, 0, 1, 0, 0, 1, 0,
-						0, 0, 0, 1, 1, 1, 0, 1, 1, 1,
-						0, 0, 0, 0, 0, 1, 1, 1, 0, 1,
-						0, 0, 0, 0, 0, 0, 0, 0, 1, 1};
+		int intArray[]={0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+						0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+						0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		
 		vector<int> giveMeIds;
 		int previousGiveMeIdsSize = giveMeIds.size();
-		cout << intArray << endl;
+		//cout << intArray << endl;
 		int placement = coordToIndex(initialCoord_x, initialCoord_y, width);
 		
 		verifyTopBottomLeftAndRight(giveMeIds, intArray, placement, width, height);
@@ -132,18 +218,16 @@ public:
 		while(giveMeIds.size() != previousGiveMeIdsSize){
 			previousGiveMeIdsSize = giveMeIds.size();
 			for (int i=giveMeIds.size()-1; i>=0; i--) {
-				cout << "Checking id: " << giveMeIds.at(i) << "." << endl;
+				//cout << "Checking id: " << giveMeIds.at(i) << "." << endl;
 				verifyTopBottomLeftAndRight(giveMeIds, intArray, giveMeIds.at(i), width, height);
 			}
-			cout << "previousGiveMeIdsSize: " << previousGiveMeIdsSize << ". " << "giveMeIds: " << giveMeIds.size() << "." << endl;
+			//cout << "previousGiveMeIdsSize: " << previousGiveMeIdsSize << ". " << "giveMeIds: " << giveMeIds.size() << "." << endl;
 		}
 		
 		cout << giveMeIds.size() << endl;
 		for (long index=0; index<(long)giveMeIds.size(); ++index) cout << giveMeIds.at(index) << " ";
 		cout << endl;
 		
-		int colomnMatch = 0;
-		int rowMatch = 0;
 		cout << endl;
 		for(int i=0; i<width*height; i++){
 		int checkerInt = (i+1)%(width);
@@ -161,26 +245,11 @@ public:
 			if(checkerInt == 0){
 				cout << endl;
 			}
-			
-			
 		}
 		cout << endl;
-		for(int i=0; i<giveMeIds.size(); i++){
-			if(initialCoord_x == indexToCoordX(giveMeIds.at(i), width)){
-				colomnMatch++;
-			}
-		}
 		
-		for(int i=0; i<giveMeIds.size(); i++){
-			if(initialCoord_y == indexToCoordY(giveMeIds.at(i), width)){
-				rowMatch++;
-			}
-		}
-		
-		
-		
-		cout << "colomnMatch: " << colomnMatch << endl;
-		cout << "rowMatch: " << rowMatch << endl;
+		cout << "colomnMatch: " << giveMeColomnMatchesForCoord(giveMeIds, initialCoord_x, initialCoord_y, width, height) << endl;
+		cout << "rowMatch: " << giveMeRowMatchesForCoord(giveMeIds, initialCoord_x, initialCoord_y, width, height) << endl;
 			
 		return giveMeIds;
 	}
